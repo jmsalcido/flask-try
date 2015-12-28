b3fa5ce28bce9060dd8d6a1b07d89718cbdfeb75
jmsalcido <eljose152@gmail.com>
2015-12-27 22:37:01 -0700

    update gitignore

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,5 +1,6 @@
 from app import db
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
@@ -9,8 +10,9 @@ class User(db.Model):
     def __repr__(self):
         return '<User {0}>'.format(self.nickname)
 
+
 class Post(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
--- microblog/app/views.py
+++ microblog/app/views.py
@@ -28,7 +28,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for OpenID="{0}", remember_me={1}'
-            .format(form.openid.data, str(form.remember_me.data)))
+              .format(form.openid.data, str(form.remember_me.data)))
         return redirect('/index')
     return render_template('login.html',
                            title='Sign In',
