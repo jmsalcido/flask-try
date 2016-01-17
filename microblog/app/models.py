15b57ea3c5bf928ee887e0fc1c92cbc74ed1f4dc
jmsalcido <eljose152@gmail.com>
2016-01-16 18:40:09 -0700

    following posts

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -58,6 +58,10 @@ class User(db.Model, UserMixin):
     def is_following(self, user):
         return self.followed.filter(followers.columns.followed_id == user.id).count() > 0
 
+    def followed_posts(self):
+        return Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter(
+            followers.c.follower_id == self.id).order_by(Post.timestamp.desc())
+
     def __repr__(self):
         return '<User {0}>'.format(self.username)
 
